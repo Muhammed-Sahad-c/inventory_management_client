540bbb9a9ef5f07abdd912c8aa761d60f714188c
Your GitHub Username <your-email@example.com>
2023-08-06 16:48:21 +0530

    style: designed login page

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react';
 import '../../assets/styles/login.css';
+import { useNavigate } from 'react-router-dom';
 import { validateLoginDetails } from '../../services/UserAuthentication';
+import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBInput } from 'mdb-react-ui-kit';
 
 function Login() {
-  const [loginDetails, setLoginDetails] = useState({ email: "", password: "" });
+  const navigate = useNavigate();
   const [process, setProcess] = useState(false);
+  const [errors, setErrors] = useState({ email: "", password: "" });
+  const [loginDetails, setLoginDetails] = useState({ email: "", password: "" });
 
   const { email, password } = loginDetails;
 
@@ -16,16 +20,38 @@ function Login() {
     }));
   };
 
+  const validateFormDetails = () => {
+    const newErrors = {};
+    if (email.trim() === "") {
+      newErrors.password = "Email cannot be empty"
+    }
+
+    if (password.trim() === "") {
+      newErrors.email = "Password cannot be empty"
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      newErrors.email = 'Invalid email format';
+    }
+
+    setErrors(newErrors)
+
+    return Object.keys(newErrors).length === 0;
+  }
+
   const submitLoginDetails = async e => {
     e.preventDefault();
     if (process === false) {
+      setErrors({ email: "", password: "" });
       setProcess(true);
-      const validate = await validateLoginDetails({ email, password });
-      const { status, message, token } = validate.data;
-      if (status) {
-        localStorage.setItem("inventory_user_token", token);
-      } else {
-        console.log(`sometign went wrong`);
+      if (validateFormDetails()) {
+        const validate = await validateLoginDetails({ email, password });
+        const { status, message, token } = validate.data;
+        if (status) {
+          localStorage.setItem("inventory_user_token", token);
+          setErrors({ email: "", password: "" });
+          navigate("/");
+        } else {
+          setErrors({ email: message, password: message });
+        }
       }
       setProcess(false);
     }
@@ -33,11 +59,32 @@ function Login() {
 
   return (
     <>
-      <form action="" onSubmit={submitLoginDetails}>
-        <input type="email" placeholder='email' name='email' value={email} onChange={onInputChange} /> <br />
-        <input type="password" placeholder='password' name='password' value={password} onChange={onInputChange} /> <br />
-        <button type='submit'>login</button>
-      </form>
+      <MDBContainer fluid className='p-3'>
+        <MDBRow className='text-center d-flex justify-content-center align-items-center '>
+          <MDBCol md='4'>
+            <MDBCard className='my-5'>
+              <MDBCardBody className='p-5'>
+                <div className="text-start py-3">
+                  <h2>Welcome back!</h2>
+                </div>
+                <MDBInput wrapperClass='' placeholder='email' id='email' type='email' size='lg' name='email' className={`rounded-0 ${errors.email ? "border-danger" : ""}`} onChange={onInputChange} />
+                <div className="text-start">
+                  <small className='text-danger'>{errors.email}</small>
+                </div> 
+                <MDBInput wrapperClass='pt-4' placeholder='password' id='password' type='password' name='password' size='lg' className={`rounded-0 ${errors.password ? "border-danger" : ""}`} onChange={onInputChange} />
+                <div className="text-start pb-4">
+                  <small className='text-danger'>{errors.password}</small>
+                </div>
+                <button className="rounded-0 w-100 py-3 border-0 bg-primary text-white rounded" onClick={submitLoginDetails}>
+                  {
+                    process ? "Loading...." : "login"
+                  }
+                </button>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import App from './App.jsx';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import { store } from './redux/store.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
